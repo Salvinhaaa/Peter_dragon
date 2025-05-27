4a053cf259c019693c6207c73d7c04cb077a5f7a
Salvinhaaa <salvadormpereira13@icloud.com>
2025-05-27 15:27:06 +0100

    faz download deste ficheiro.bat

--- faz download deste ficheiro.bat
+++ faz download deste ficheiro.bat
@@ -8,11 +8,11 @@ cls
 chcp 65001 >nul 2>&1
 cls 
 :MENU
-echo.                 %b%"═══════════════════════════════════════════════════════════════════════════════"%w%
-echo.                                                
+echo.                 %b%"═══════════════════════════════════════════════════════════════════════════════"%w%                             
 echo.                 %p%[%w%1%p%]%w% Dowload do NET     
 echo.                 %p%[%w%2%p%]%w% Dowload do Github                                   
 echo.                 %p%[%w%M%p%]%w% Sair
+echo.                 %p%[%w%h%p%]%w% Verificar ficheiros
 echo.                 %b%"═══════════════════════════════════════════════════════════════════════════════"%w%
 echo.                  %W%Faz primeiro dowload do NET independemente do PC
 echo.                 %W% Depois executa o Dowload do Github
@@ -22,16 +22,88 @@ set /p input=:
 if /i %input% == 1 goto NET
 if /i %input% == 2 goto GitHub
 if /i %input% == m goto sair
- 
+if /i %input% == h goto Verificar
+
+:Verificar
+@echo off
+cls
+color 5
+
+echo.
+echo.
+echo [====                         7.0%                          ]
+echo.
+timeout /t 2 /nobreak > NUL
+cls
+
+echo.
+echo.
+echo [=============================85.0%===================     ]
+echo.
+timeout /t 5 /nobreak > NUL
+cls
+
+echo.
+echo.
+echo [=============================100.0%=======================]
+echo %w%- Acabou! %b%
+
+cls
+goto MENU
+
 :GitHub
 @echo off
 cls
+:MENU1
+echo.                 %b%"═══════════════════════════════════════════════════════════════════════════════"%w%                                       
+echo.                 %p%[%w%1%p%]%w% .RAR     
+echo.                 %p%[%w%2%p%]%w% .ZIP                               
+echo.                 %p%[%w%V%p%]%w% Sair
+echo.                 %p%[%w%X%p%]%w% Voltar ao menu
+echo.                 %b%"═══════════════════════════════════════════════════════════════════════════════"%w%
+echo.                  %W% .RAR/.ZIP
+echo.                 %b%"═══════════════════════════════════════════════════════════════════════════════"%w%
+set /p input=:
+if /i %input% == 1 goto winrar
+if /i %input% == 2 goto zipado
+if /i %input% == v goto sair
+if /i %input% == x goto bazar
 
-:: Reinicia o script em uma janela maximizada, se necessário
-if not "%1" == "max" (
-    start /MAX cmd /c "%~f0" max
-    exit /b
-)
+:zipado
+cls 
+
+:: Caminhos
+set "DEST=C:\FILME_PETER_DRAGON"
+set "RAR=%temp%\resource.zip"
+set "URL=https://github.com/Salvinhaaa/Peter_dragon/releases/download/movue/resource.zip"
+
+:: Cria a pasta de destino, se não existir
+md "%DEST%" 2>nul
+
+:: Mensagem informativa
+powershell -Command ^
+  "Add-Type -AssemblyName System.Windows.Forms; " ^
+  "[System.Windows.Forms.MessageBox]::Show('Download do ficheiro .zip necessario.', 'Peter_Dragon Github', [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)"
+
+:: Baixa o arquivo .RAR
+echo.
+echo A fazer o dowload dos arquivos .ZIP do GitHub...
+curl -L -o "%RAR%" "%URL%"
+
+:: Move o .RAR para a pasta de destino
+move /Y "%RAR%" "%DEST%\resource.zip"
+
+echo.
+echo [OK] Arquivo transferido para: %DEST%\resource.zip
+
+cls
+powershell -Command ^
+  "Add-Type -AssemblyName System.Windows.Forms; " ^
+  "[System.Windows.Forms.MessageBox]::Show('Clicka Ok para ir para o menu para sair.', 'GitHub dowload', [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)"
+goto MENU1
+ 
+:winrar
+cls
 
 :: Caminhos
 set "DEST=C:\FILME_PETER_DRAGON"
@@ -61,6 +133,10 @@ cls
 powershell -Command ^
   "Add-Type -AssemblyName System.Windows.Forms; " ^
   "[System.Windows.Forms.MessageBox]::Show('Clicka Ok para ir para o menu para sair.', 'GitHub dowload', [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)"
+goto MENU1
+
+:bazar
+cls
 goto MENU
 
 :sair
@@ -70,7 +146,6 @@ powershell -Command ^
   "Add-Type -AssemblyName System.Windows.Forms; " ^
   "[System.Windows.Forms.MessageBox]::Show('Click no OK para sair', 'sair', [System.Windows.Forms.MessageBoxButtons]::Ok, [System.Windows.Forms.MessageBoxIcon]::Information)"
 exit
-
 
 :NET
 cls
